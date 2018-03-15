125ebed5e74f1d8c5971e7011101a8be8173b4e7
Eshan Halekote <ehalekote@Eshans-MBP-3.lan>
2018-03-14 19:28:06 -0600

    Added CritterWorld Class

---
+++ src/assignment4/CritterWorld.java
@@ -0,0 +1,10 @@
+package assignment4;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class CritterWorld {
+	
+	ArrayList<ArrayList<ArrayList<Critter>>> myList = new ArrayList<ArrayList<ArrayList<Critter>>>();		//Represents the game board, indexed as X, Y, list of critters in the spot
+}
