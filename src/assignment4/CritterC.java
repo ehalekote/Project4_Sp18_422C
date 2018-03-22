b3976b9e25fe419f63304884c3b5f1aae758da49
Eshan Halekote <ehalekote@wireless-10-145-46-100.public.utexas.edu>
2018-03-22 18:35:41 -0500

    Updated runstats for custom critters

--- src/assignment4/CritterC.java
+++ src/assignment4/CritterC.java
@@ -45,5 +45,20 @@ public class CritterC extends Critter{
 	
 	@Override
 	public String toString() { return "&"; }
+	
+	public static void runStats(java.util.List<Critter> critterList) {
+		System.out.print("" + critterList.size() + " total CritterCs");	
+		
+		int totalWalks = 0;
+		int totalRuns = 0;
+		for (Object obj : critterList) {
+			CritterC c = (CritterC) obj;
+			totalWalks = totalWalks + c.numWalks;
+			totalRuns = totalRuns + c.numRuns;
+		}
+		System.out.print("" + totalWalks + " total number of walks");
+		System.out.print("" + totalRuns + " total number of runs");		
+		
+	}
 
 }
